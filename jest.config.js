e2c5444b7fb534a0498737618d5d7c7032d8c0fb
shoynoi <shoynoi.jp@gmail.com>
2021-08-24 08:40:22 +0900

    add @emotion/jest

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,6 @@ module.exports = {
   moduleNameMapper: {
     '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js')
-  }
+  },
+  snapshotSerializers: ['@emotion/jest/serializer'],
 }
