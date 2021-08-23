442ce9dd81133a0a9317961807e53ea451a2e479
shoynoi <shoynoi.jp@gmail.com>
2021-08-23 23:28:20 +0900

    Webpack の resolve.modules と同じ設定を Jest の moduleDirectories に記述
    - Jest においても相対パスで import されていないモジュールを解決できるようにする

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
+const path = require('path')
+
 module.exports = {
+  moduleDirectories: ['node_modules', path.join(__dirname, 'src'), 'shared'],
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '\\.module\\.css$': 'identity-obj-proxy',
