df14abfbb826073079e6c64377138de5c99a66e8
shoynoi <shoynoi.jp@gmail.com>
2021-08-24 07:38:02 +0900

    setupFilesAfterEnv でテスト環境の設定後に実行されるファイルを指定
    - @testing-library/jest-dom/extend-expect を読み込んでおく

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = {
   moduleDirectories: ['node_modules', path.join(__dirname, 'src'), 'shared'],
   testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   moduleNameMapper: {
     '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js')
