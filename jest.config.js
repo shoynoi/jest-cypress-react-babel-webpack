624dbe3a8846a6ec5a1a9c678a182e1cd3448fe5
shoynoi <shoynoi.jp@gmail.com>
2021-08-28 15:45:38 +0900

    moduleDirectories にテストユーティリティファイルを置いたディレクトリを指定しておくことで、相対パスでインポートする必要がなくなり便利

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,17 @@
 const path = require('path')
 
 module.exports = {
-  moduleDirectories: ['node_modules', path.join(__dirname, 'src'), 'shared'],
+  moduleDirectories: [
+    'node_modules',
+    path.join(__dirname, 'src'),
+    'shared',
+    path.join(__dirname, 'test'),
+  ],
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   moduleNameMapper: {
     '\\.module\\.css$': 'identity-obj-proxy',
-    '\\.css$': require.resolve('./test/style-mock.js')
+    '\\.css$': require.resolve('./test/style-mock.js'),
   },
   snapshotSerializers: ['@emotion/jest/serializer'],
 }
