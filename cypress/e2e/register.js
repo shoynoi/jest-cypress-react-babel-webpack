3e2287027b42f1828603f3d15cf688a6b3c851b9
shoynoi <shoynoi.jp@gmail.com>
2021-09-06 08:24:15 +0900

    エラー時のE2Eテストを追加

--- cypress/e2e/register.js
+++ cypress/e2e/register.js
@@ -14,4 +14,17 @@ describe('registration', () => {
       .should('be.a', 'string')
     cy.findByTestId('username-display').should('have.text', user.username)
   })
+
+  it("should show an error message if there's an error registering", () => {
+    cy.server()
+    cy.route({
+      method: 'POST',
+      url: 'http://localhost:3000/register',
+      status: 500,
+      response: {},
+    })
+    cy.visit('/register')
+    cy.findByRole('button', {name: /submit/i}).click()
+    cy.findByText(/error.*try again/i)
+  })
 })
