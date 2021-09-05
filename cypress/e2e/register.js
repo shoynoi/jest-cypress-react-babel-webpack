55ec1094979280b28197e32b0d50a3daeb1caced
shoynoi <shoynoi.jp@gmail.com>
2021-09-05 22:52:55 +0900

    ユーザー名を表示するように変更

--- cypress/e2e/register.js
+++ cypress/e2e/register.js
@@ -12,5 +12,6 @@ describe('registration', () => {
     cy.window()
       .its('localStorage.token')
       .should('be.a', 'string')
+    cy.findByTestId('username-display').should('have.text', user.username)
   })
 })
