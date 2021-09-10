da7cc8d263de027b7ba22b52b3f3b905be904b0b
shoynoi <shoynoi.jp@gmail.com>
2021-09-10 16:56:37 +0900

    ログインテストを追加

---
+++ cypress/e2e/login.js
@@ -0,0 +1,24 @@
+import {buildUser} from '../support/generator'
+
+describe('login', () => {
+  it('should login an existing user', () => {
+    const user = buildUser()
+    cy.visit('/')
+    cy.findByRole('link', {name: /register/i}).click()
+    cy.findByLabelText(/username/i).type(user.username)
+    cy.findByLabelText(/password/i).type(user.password)
+    cy.findByRole('button', {name: /submit/i}).click()
+    cy.findByRole('button', {name: /logout/i}).click()
+
+    cy.findByRole('link', {name: /login/i}).click()
+    cy.findByLabelText(/username/i).type(user.username)
+    cy.findByLabelText(/password/i).type(user.password)
+    cy.findByRole('button', {name: /submit/i}).click()
+
+    cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+    cy.window()
+      .its('localStorage.token')
+      .should('be.a', 'string')
+    cy.findByTestId('username-display').should('have.text', user.username)
+  })
+})
