57bd2688309b2f2b8a9a179a91a19421ad8d92b8
shoynoi <shoynoi.jp@gmail.com>
2021-09-10 17:27:55 +0900

    コマンドを追加

--- cypress/e2e/login.js
+++ cypress/e2e/login.js
@@ -1,23 +1,17 @@
-import {buildUser} from '../support/generator'
-
 describe('login', () => {
   it('should login an existing user', () => {
-    const user = buildUser()
-    cy.request({
-      url: 'http://localhost:3000/register',
-      method: 'POST',
-      body: user,
-    })
-    cy.visit('/')
-    cy.findByRole('link', {name: /login/i}).click()
-    cy.findByLabelText(/username/i).type(user.username)
-    cy.findByLabelText(/password/i).type(user.password)
-    cy.findByRole('button', {name: /submit/i}).click()
+    cy.createUser().then(user => {
+      cy.visit('/')
+      cy.findByRole('link', {name: /login/i}).click()
+      cy.findByLabelText(/username/i).type(user.username)
+      cy.findByLabelText(/password/i).type(user.password)
+      cy.findByRole('button', {name: /submit/i}).click()
 
-    cy.url().should('eq', `${Cypress.config().baseUrl}/`)
-    cy.window()
-      .its('localStorage.token')
-      .should('be.a', 'string')
-    cy.findByTestId('username-display').should('have.text', user.username)
+      cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+      cy.window()
+        .its('localStorage.token')
+        .should('be.a', 'string')
+      cy.findByTestId('username-display').should('have.text', user.username)
+    })
   })
 })
